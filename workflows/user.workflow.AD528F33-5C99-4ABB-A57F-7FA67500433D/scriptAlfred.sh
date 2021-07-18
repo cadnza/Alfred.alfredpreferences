5032e7c5bd950240bd4a9a510e883607d36cbec4
Jon Dayley <jonathandayley@gmail.com>
2021-07-18 15:34:48 -0600

    Add entire Alfred repo as choice under Alfred workflows

--- workflows/user.workflow.AD528F33-5C99-4ABB-A57F-7FA67500433D/scriptAlfred.sh
+++ workflows/user.workflow.AD528F33-5C99-4ABB-A57F-7FA67500433D/scriptAlfred.sh
@@ -1,13 +1,31 @@
 # Get Alfred directory
-dirAlfred=~/Repos/Alfred.alfredpreferences/workflows
+dirAlfred=~/Repos/Alfred.alfredpreferences
+
+# Get workflows directory
+dirWorkflows=$dirAlfred/workflows
+
+# Get Alfred icon path
+iconPathAlfred="/Applications/Alfred 4.app/Contents/Resources/appicon.icns"
 
 # Open JSON variable
 json=""
 
+# Add item to modify entire repo
+repoItem="{
+	\"title\": \"Alfred.alfredpreferences\",
+	\"subtitle\": \"$dirAlfred\",
+	\"arg\": \"$dirAlfred\",
+	\"icon\": {\"path\":\"$iconPathAlfred\"},
+	\"autocomplete\": \"$dirAlfred\",
+	\"text\": \"$dirAlfred\",
+	\"quicklookurl\": \"$dirAlfred\"
+}"
+json=$json,$repoItem
+
 # Open main loop through workflows
 ls -1 ~/Repos/Alfred.alfredpreferences/workflows | while read -r dirWkflw
 do
-	fullpath=$dirAlfred/$dirWkflw
+	fullpath=$dirWorkflows/$dirWkflw
 	plist=$fullpath/info.plist
 	title=$(defaults read $plist name)
 	subtitle=$(defaults read $plist description)
