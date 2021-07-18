7895390b1648943a6eaa9ba7a5651d230546555d
Jon Dayley <jonathandayley@gmail.com>
2021-07-18 14:30:15 -0600

    Initial commit

---
+++ workflows/user.workflow.C6738EC1-E9A2-4C1F-9D92-181C5B1CECF6/script.sh
@@ -0,0 +1,41 @@
+# Get repo directory
+dirRepos=$1
+
+# Get folder icon path
+iconPath="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericFolderIcon.icns"
+
+# Open JSON variable
+json=""
+
+# Open main loop through repos
+ls $dirRepos | while read -r repo
+do
+	fullpath=$dirRepos/$repo
+	filterpattern="\.Rproj$"
+	candidates=$(ls -1 $fullpath | grep -i $filterpattern)
+	hasRproj=$(echo $candidates | grep -c .)
+	[ $hasRproj = 0 ] && continue
+	title=$repo
+	subtitle=$fullpath
+	arg=$fullpath/$candidates
+	autocomplete=$repo
+	text=$fullpath
+	quicklookurl=$fullpath
+	newItem="{
+		\"title\": \"$title\",
+		\"subtitle\": \"$subtitle\",
+		\"arg\": \"$arg\",
+		\"icon\": {\"path\":\"$iconPath\"},
+		\"autocomplete\": \"$autocomplete\",
+		\"text\": \"$text\",
+		\"quicklookurl\": \"$quicklookurl\"
+	}"
+	json=$json,$newItem
+done
+
+# Remove final comma
+json="${json:1}"
+
+# Frame final JSON
+final="{\"items\": [$json]}"
+echo -n $final
