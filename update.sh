d0e7d29b112904429e5ab0c1cef16e3cd819232c
Jon Dayley <jonathandayley@gmail.com>
2021-07-21 08:04:27 -0600

    Organize comments

--- update.sh
+++ update.sh
@@ -35,13 +35,16 @@ iconExtensionTarget=png
 # Set icon width (unitless)
 iconWidth=100
 
-# Get workflow metadata
+# Open workflow loop
 echo $workflows | while read -r workflow
 do
+
 	# Set workflow directory
 	dirWorkflow=$dirWorkflows/$workflow
+
 	# Get plist
 	plist=$dirWorkflow/info.plist
+
 	# Get workflow metadata
 	name=$(defaults read $plist name)
 	createdby=$(defaults read $plist createdby)
@@ -49,8 +52,10 @@ do
 	version=$(defaults read $plist version) 2> /dev/null
 	description=$(defaults read $plist description)
 	bundleid=$(defaults read $plist bundleid)
+
 	# Get asset name
 	[[ ${#bundleid} = 0 ]] && assetName=$(echo $name | sed 's/[^A-z0-9]//g') || assetName=$bundleid
+
 	# Get workflow icon
 	iconPre=$dirWorkflow/$iconFileTarget
 	if [[ -f "$iconPre" ]]
@@ -77,16 +82,19 @@ do
 	else
 		icon=""
 	fi
+
 	# Format version
 	if [[ ${#version} -gt 0 ]]
 	then
 		version=\`$version\`
 	fi
+
 	# Add https prefix to web address if not present
 	if [[ $(echo $webaddress | grep -Ec ^https?://) = 0 && ${#webaddress} != 0 ]]
 	then
 		webaddress=https://$webaddress
 	fi
+
 	# Sort author
 	if [[ ${#createdby} = 0 ]]
 	then
@@ -94,15 +102,20 @@ do
 	else
 		[[ ${#webaddress} = 0 ]] && author=$createdby || author=[$createdby]($webaddress)
 	fi
+
 	# Add row to markdown table
 	md=$md'\n'
 	newRow="| $icon | **$name** | $version | $author | $description |"
 	md=$md$newRow
+
+# Close workflow loop
 done
 
+# Write updates to readme
+wildcard="%workflows%"
 echo $template | while read -r line
 do
-	if [[ $line = "%workflows%" ]]
+	if [[ $line = $wildcard ]]
 	then
 		echo $md >> $readme
 	else
