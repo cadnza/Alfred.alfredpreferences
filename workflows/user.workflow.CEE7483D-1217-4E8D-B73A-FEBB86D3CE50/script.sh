830b5317fa88d3faef709017b42c92b71b396772
Jon Dayley <jonathandayley@gmail.com>
2021-07-22 14:39:06 -0700

    Fix bugs in Lego handling script

--- workflows/user.workflow.CEE7483D-1217-4E8D-B73A-FEBB86D3CE50/script.sh
+++ workflows/user.workflow.CEE7483D-1217-4E8D-B73A-FEBB86D3CE50/script.sh
@@ -1,7 +1,7 @@
 /Library/Frameworks/R.framework/Resources/bin/Rscript partsearch.R $1 2> /dev/null || {
 	title="Install R via Homebrew"
 	link="https://www.r-project.org/"
-	rMessage="{
+	rMessage="{\"items\": [{
 		\"title\": \"$title\",
 		\"subtitle\": \"R installation not found\",
 		\"arg\": \"noR\",
@@ -9,6 +9,6 @@
 		\"autocomplete\": \"$title\",
 		\"text\": \"$title\",
 		\"quicklookurl\": \"$dirAlfred\"
-	}"
-	echo rMessage
+	}]}"
+	echo $rMessage
 }
