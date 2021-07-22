9db24f1e3b79b225a371e3d4f32fe844c462e7ac
Jon Dayley <jonathandayley@gmail.com>
2021-07-22 14:36:43 -0700

    Outsource main Lego script running routine to handling script for ease of error handling

---
+++ workflows/user.workflow.CEE7483D-1217-4E8D-B73A-FEBB86D3CE50/script.sh
@@ -0,0 +1,14 @@
+/Library/Frameworks/R.framework/Resources/bin/Rscript partsearch.R $1 2> /dev/null || {
+	title="Install R via Homebrew"
+	link="https://www.r-project.org/"
+	rMessage="{
+		\"title\": \"$title\",
+		\"subtitle\": \"R installation not found\",
+		\"arg\": \"noR\",
+		\"icon\": {\"path\":\"$dirAlfred\",\"type\":\"fileicon\"},
+		\"autocomplete\": \"$title\",
+		\"text\": \"$title\",
+		\"quicklookurl\": \"$dirAlfred\"
+	}"
+	echo rMessage
+}
