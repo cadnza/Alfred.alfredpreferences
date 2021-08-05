a2abac70a2617a98deebc3602a4137fd7e597f13
Jon Dayley <jonathandayley@gmail.com>
2021-08-05 11:52:10 -0600

    Add files for Brave bookmarks workflow

---
+++ workflows/user.workflow.76D44DD1-43A5-40AF-9CBF-1BE229BE0C2B/script.sh
@@ -0,0 +1,57 @@
+typeset -F SECONDS=0 #TEMP
+
+# Identify target directory
+braveDir="/Users/cadnza/Library/Application Support/BraveSoftware/Brave-Browser" # Change to ~ #TEMP
+
+# Get metadata file
+braveFile="$braveDir/Local State"
+
+# Convert to JSON
+jsonMaster=$(cat $braveFile | jq .)
+
+# Subset JSON
+jsonSubset=$(cat $braveFile | jq '.profile.info_cache | to_entries[]')
+
+# Get profile directories
+profileDirs=$(echo $jsonSubset | jq -r '.key')
+
+# Get last used profile directory
+profileDirBase=$(echo $jsonMaster | jq -r '.profile.last_used')
+
+# Subset JSON for last used profile directory
+jsonProfile=$(echo $jsonSubset | jq "select(.key == \"$profileDirBase\")")
+
+# Get name
+name=$(echo $jsonProfile | jq -r '.value.name')
+
+# Get icon
+icon=$(echo $jsonProfile | jq -r '.value.avatar_icon')
+
+# Get directory for focus profile
+profileDir=$braveDir/$profileDirBase
+
+# Get bookmarks file
+bookmarks=$profileDir/bookmarks
+
+# Open variable for final JSON
+jsonFinal=""
+
+# Get GUIDs of folder nodes
+guids=$(cat $bookmarks | jq -r '.. | objects | select(.type == "folder") | .guid')
+
+# Open loop for folders
+echo $guids | while read -r guid
+do
+
+	# Get URL nodes belonging to folder
+	selector=".. | objects | select(.guid == \"$guid\") | .. | objects | select(has(\"url\")) | .guid"
+	guidsURL=$(cat $bookmarks | jq -c $selector)
+
+# Close loop
+done
+
+
+
+
+echo
+echo "$SECONDS" #TEMP
---
+++ workflows/user.workflow.76D44DD1-43A5-40AF-9CBF-1BE229BE0C2B/updateDatabase.sh
@@ -0,0 +1,87 @@
+# Identify target directory
+braveDir="/Users/cadnza/Library/Application Support/BraveSoftware/Brave-Browser" # Change to ~ #TEMP
+
+# Get metadata file
+braveFile="$braveDir/Local State"
+
+# Convert to JSON
+jsonMaster=$(cat $braveFile | jq .)
+
+# Subset JSON for profiles
+jsonSubsets=$(cat $braveFile | jq -c '.profile.info_cache | to_entries[]')
+
+# Open loop for profiles
+echo $jsonSubsets | while read -r jsonSubset
+do
+
+	# Get profile directory
+	profileDir=$(echo $jsonSubset | jq -r '.key')
+
+	# Subset JSON for profile directory
+	jsonProfile=$(echo $jsonSubset | jq "select(.key == \"$profileDir\")")
+
+	# Get profile name
+	d_profile=$(echo $jsonProfile | jq -r '.value.name')
+
+	# Get icon
+	d_icon=$(echo $jsonProfile | jq -r '.value.avatar_icon')
+
+	# Get directory for focus profile
+	profileDir=$braveDir/$profileDir
+
+	# Get bookmarks file
+	bookmarks=$profileDir/bookmarks
+
+	# Convert bookmarks file to JSON
+	jsonBm=$(cat $bookmarks | jq .)
+
+	# Get root node
+	jsonRoot=$(echo $jsonBm | jq -r '.roots')
+
+	# Get nodes containing url attributes
+	urlPaths=$(echo $jsonRoot | jq -rc 'paths | select(.[-1] == "url") | del(. | last) | join(".")')
+
+	# Open paths loop
+	echo $urlPaths | while read -r urlPath
+	do
+
+		# Format node path
+		urlPath=$(
+			echo .$urlPath |
+			sed 's/\s*$//g' |
+			perl -pe 's/\.(?=\d+)/[/g' |
+			perl -pe 's/(?<=\d)(?=\.)/]/g' |
+			perl -pe 's/(?<=\d)$/]/g'
+		)
+
+		# Get bookmark name
+		selector=$urlPath".name"
+		d_name=$(echo $jsonRoot | jq -r $selector)
+
+		# Get URL
+		selector=$urlPath".url"
+		d_url=$(echo $jsonRoot | jq -r $selector)
+
+		# Get display path
+		urlPathSplit=$(echo $urlPath | perl -pe 's/(?<=.)\./\n./g')
+		urlPath=""
+		displayPath=""
+		echo $urlPathSplit | while read -r emt
+		do
+			urlPath=$urlPath$emt
+			emtName=$(echo $jsonRoot | jq -r $urlPath | jq -r '.name')
+			emtType=$(echo $jsonRoot | jq -r $urlPath | jq -r '.type')
+			[[ $emtType = folder ]] && displayPath="$displayPath / $emtName"
+		done
+		displayPath="[ $(echo $displayPath | perl -pe 's/^\s*\/\s*//') ]"
+
+		# Get subtitle
+		d_subtitle="$displayPath $d_url"
+
+		echo $displayPath #TEMP
+
+	# Close paths loop
+	done
+
+# Close profiles loop
+done
