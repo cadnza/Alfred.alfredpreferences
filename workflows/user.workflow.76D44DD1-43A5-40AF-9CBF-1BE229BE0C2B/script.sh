cd60339feb9e007200f0399f5b662fa4268f639f
Jon Dayley <jonathandayley@gmail.com>
2021-08-24 17:36:15 -0600

    Remove redundant code

--- workflows/user.workflow.76D44DD1-43A5-40AF-9CBF-1BE229BE0C2B/script.sh
+++ workflows/user.workflow.76D44DD1-43A5-40AF-9CBF-1BE229BE0C2B/script.sh
@@ -6,46 +6,5 @@ braveDir="$HOME/Library/Application Support/BraveSoftware/Brave-Browser"
 # Get metadata file
 braveFile="$braveDir/Local State"
 
-# Convert to JSON
-jsonMaster=$(cat $braveFile | jq .)
-
-# Subset JSON
-jsonSubset=$(cat $braveFile | jq '.profile.info_cache | to_entries[]')
-
-# Get profile directories
-profileDirs=$(echo $jsonSubset | jq -r '.key')
-
 # Get last used profile directory
-profileDirBase=$(echo $jsonMaster | jq -r '.profile.last_used')
-
-# Subset JSON for last used profile directory
-jsonProfile=$(echo $jsonSubset | jq "select(.key == \"$profileDirBase\")")
-
-# Get name
-name=$(echo $jsonProfile | jq -r '.value.name')
-
-# Get icon
-icon=$(echo $jsonProfile | jq -r '.value.avatar_icon')
-
-# Get directory for focus profile
-profileDir=$braveDir/$profileDirBase
-
-# Get bookmarks file
-bookmarks=$profileDir/bookmarks
-
-# Open variable for final JSON
-jsonFinal=""
-
-# Get GUIDs of folder nodes
-guids=$(cat $bookmarks | jq -r '.. | objects | select(.type == "folder") | .guid')
-
-# Open loop for folders
-echo $guids | while read -r guid
-do
-
-	# Get URL nodes belonging to folder
-	selector=".. | objects | select(.guid == \"$guid\") | .. | objects | select(has(\"url\")) | .guid"
-	guidsURL=$(cat $bookmarks | jq -c $selector)
-
-# Close loop
-done
+lastProfile=$(cat $braveFile | jq -r '.profile.last_used')
