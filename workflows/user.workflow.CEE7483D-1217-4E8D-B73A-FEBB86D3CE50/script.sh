feb3af2820c9eb913128545a7deb307c66064095
Jon Dayley <jonathandayley@gmail.com>
2021-08-31 07:50:59 -0600

    Refactor main script to use jq

--- workflows/user.workflow.CEE7483D-1217-4E8D-B73A-FEBB86D3CE50/script.sh
+++ workflows/user.workflow.CEE7483D-1217-4E8D-B73A-FEBB86D3CE50/script.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env zsh
 
-./partsearch.R $1 2> /dev/null || {
-	title="Install R via Homebrew"
-	link="https://www.r-project.org/"
-	rMessage="{\"items\": [{
-		\"title\": \"$title\",
-		\"subtitle\": \"R installation not found\",
-		\"arg\": \"noR\",
-		\"icon\": {\"path\":\"images/R.png\",},
-		\"autocomplete\": \"$title\",
-		\"text\": \"$title\",
-		\"quicklookurl\": \"$dirAlfred\"
-	}]}"
+# Add /usr/local/bin to path
+PATH=/usr/local/bin:$PATH
+
+# Search for part
+./partsearch.R $1
+[[ $? = 127 ]] && {
+	rMessage=$(
+		jq -nc \
+			--arg link "https://www.r-project.org/" \
+			'{
+				"items": [
+					{
+						"title": "R installation not found",
+						"subtitle": "Please install R to continue.",
+						"arg": $link,
+						"icon": {"path": "images/R.png"},
+						"quicklookurl": $link
+					}
+				]
+			}'
+	)
 	echo $rMessage
 }
