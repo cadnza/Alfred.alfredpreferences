9d4d9cda08f7e9fcdf26c268baf5e88abd8f0902
Jon Dayley <jonathandayley@gmail.com>
2023-01-09 14:54:44 -0800

    Start move to script filter to show multiple results

--- workflows/user.workflow.86FC7E37-F008-4CD2-9B12-FF24D6E85A4F/calculate.R
+++ workflows/user.workflow.86FC7E37-F008-4CD2-9B12-FF24D6E85A4F/calculate.R
@@ -18,24 +18,61 @@ for(x in varsVector){
 }
 
 # Calculate amount ----
+ptrnC <- "c"
 amount <- ifelse(
-	grepl("c",vars$n,ignore.case=TRUE),
+	grepl(ptrnC,vars$n,ignore.case=TRUE),
 	vars$p*exp(vars$r*vars$t),
 	vars$p*(1+vars$r/vars$n)^(vars$n*vars$t)
 )
 
-# Round amount ----
-amount <- round(amount,2)
+# Calculate amount per period at rate ----
+amountPerPeriod <- amount*vars$r
 
-# Cast amount ----
-amount <- as.character(amount)
+# Define function to format currency ----
+curr <- function(x,prty=TRUE)
+	paste0(
+		ifelse(prty,"$",""),
+		formatC(
+			x,
+			big.mark=ifelse(prty,",",""),
+			format="f",
+			digits=2
+		)
+	)
 
-# Add trailing zero if missing ----
-if(grepl("^\\d+\\.\\d$",amount))
-	amount <- paste0(amount,"0")
+# Format amounts ----
+amount <- curr(amount,FALSE)
+amountPerPeriod <- curr(amountPerPeriod,FALSE)
+
+# Assemble JSON ----
+reportSep <- " | "
+accumulateString <- ifelse(
+	grepl(ptrnC,vars$n,ignore.case=TRUE),
+	"Accumulates continuously",
+	glue::glue("Accumulates {vars$n} times per period")
+
+)
+report <- glue::glue("{curr(vars$p)}{reportSep}{vars$r}{reportSep}{vars$t} periods{reportSep}{accumulateString}")
+final <- jsonlite::toJSON(
+	list(
+		items=list(
+			list(
+				title=amount,
+				subtitle=glue::glue("Amount{reportSep}{report}")
+				# ROAD WORK #TEMP
+			),
+			list(
+				title=amountPerPeriod,
+				subtitle=glue::glue("Per period{reportSep}{report}")
+				# ROAD WORK #TEMP
+			)
+		)
+	),
+	auto_unbox=TRUE
+)
 
 # Return ----
-cat(amount)
+cat(final)
 
 # Exit ----
 quit("no",0)
