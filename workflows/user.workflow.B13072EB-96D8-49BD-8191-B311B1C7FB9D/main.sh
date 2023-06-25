c330a02ed24edf95c17f35e374f5126bc8fce2a2
Jon Dayley <jonathandayley@gmail.com>
2023-06-25 15:58:10 -0600

    Add Steam workflow

---
+++ workflows/user.workflow.B13072EB-96D8-49BD-8191-B311B1C7FB9D/main.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env zsh
+
+# Set Steam games directory
+gamesDir="$HOME/Library/Application Support/Steam/steamapps/common"
+
+# Validate directory
+[[ -d $gamesDir ]] || {
+	final=$(
+		jq -n --arg gamesDir "$gamesDir" '{
+			items: [
+				{
+					title: "Directory not found",
+					subtitle: $gamesDir,
+					valid: false
+				}
+			]
+		}'
+	)
+	echo $final
+	exit 0
+}
+
+# Initialize JSON
+final=$(echo '[]' | jq)
+
+# Build JSON
+find $gamesDir \
+	-mindepth 2 \
+	-maxdepth 2 \
+	-type d \
+	-name "*.app" \
+	| while read -r f
+do
+	final=$(
+		echo $final | \
+		jq \
+		--arg title "$(basename $f | sed 's/\.app$//g')" \
+		--arg original $f \
+		--arg identifier "$(mdls -r $f -attr kMDItemCFBundleIdentifier)" \
+		'
+			. |= . + [
+				{
+					title: $title,
+					subtitle: $identifier,
+					arg: $original,
+					icon: {
+						type: "fileicon",
+						path: $original
+					},
+					type: "file:skipcheck",
+					text: $title,
+					quicklookurl: $original
+				}
+			]
+		'
+	)
+done
+
+# Finalize JSON
+final=$(echo $final | jq '{items: .}')
+
+# Return
+echo -n $final
+
+# Exit
+exit 0
