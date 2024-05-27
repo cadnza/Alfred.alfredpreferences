34b452b3b38686b82612341fd6adbead45dde45d
Jon Dayley <jonathandayley@gmail.com>
2024-05-27 10:12:40 -0600

    Fix newline bug in bookmarks

--- workflows/user.workflow.D860DEDF-A242-449C-A028-9C247158C04B/idToURL.sh
+++ workflows/user.workflow.D860DEDF-A242-449C-A028-9C247158C04B/idToURL.sh
@@ -5,7 +5,7 @@ PATH=/usr/local/bin:$PATH
 PATH=$HOME/.local/bin:$PATH
 
 # Get URL
-bookmarks list -j | jq -r ".[] | select(.id == $id).url"
+bookmarks list -j | jq -jr ".[] | select(.id == $id).url"
 
 # Exit
 exit 0
