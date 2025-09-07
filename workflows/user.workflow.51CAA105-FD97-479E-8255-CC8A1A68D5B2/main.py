8da34e9d62bd1719dc94335577286dd8f618186f
Jon Dayley <jonathandayley@gmail.com>
2025-09-06 19:23:59 -0600

    Format

--- workflows/user.workflow.51CAA105-FD97-479E-8255-CC8A1A68D5B2/main.py
+++ workflows/user.workflow.51CAA105-FD97-479E-8255-CC8A1A68D5B2/main.py
@@ -4,20 +4,16 @@ import json
 import os
 import re
 import sys
-import tomllib
 from pathlib import Path
 from typing import Any, Dict
 
+import tomllib
+
 # Get crate source directory
-d_src = Path(
-    f"{os.getenv('HOME')}/.cargo/registry/src"
-)
+d_src = Path(f"{os.getenv('HOME')}/.cargo/registry/src")
 d_crates = Path(
-    d_src / [
-        d
-        for d in os.listdir(d_src)
-        if re.compile(r"^index\.crates\.io").match(d)
-    ][0]
+    d_src
+    / [d for d in os.listdir(d_src) if re.compile(r"^index\.crates\.io").match(d)][0]
 )
 
 
@@ -34,27 +30,17 @@ def parse_toml(path: Path) -> Dict[str, Any]:
             "title": pkg["name"],
             "subtitle": f"{pkg['version']} - {pkg['description']}",
             "arg": url,
-            "icon": {
-                "path": "icon.png"
-            },
+            "icon": {"path": "icon.png"},
             "autocomplete": pkg["name"],
-            "text": {
-                "copy": pkg["name"],
-                "largetype": pkg["description"]
-            },
-            "quicklookurl": url
+            "text": {"copy": pkg["name"], "largetype": pkg["description"]},
+            "quicklookurl": url,
         }
 
 
 # Get crate data
 crates = [
     parse_toml(toml)
-    for toml
-    in [
-        Path(d_crates / d / "Cargo.toml")
-        for d
-        in os.listdir(d_crates)
-    ]
+    for toml in [Path(d_crates / d / "Cargo.toml") for d in os.listdir(d_crates)]
 ]
 
 # Return
