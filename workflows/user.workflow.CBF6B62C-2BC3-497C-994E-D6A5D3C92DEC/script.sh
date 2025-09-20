75be26f086dfdb538c721f9651ee58fdd1050241
Jon Dayley <jonathandayley@gmail.com>
2025-09-20 14:33:45 -0600

    Add positron workflow

---
+++ workflows/user.workflow.CBF6B62C-2BC3-497C-994E-D6A5D3C92DEC/script.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Add to path
+PATH=/usr/local/bin:$PATH
+PATH=/opt/homebrew/bin:$PATH
+
+# Get repo directory
+dirRepos=$1
+
+# Open JSON variable
+json=""
+
+# Open main loop through repos
+allRepos=$(eval "$(echo ls -d "$(echo "$dirRepos" | sed 's/\/*$//')"/\*/)")
+while read -r repoRaw; do
+    newItem=$(
+        jq -nc \
+            --arg repo "$(basename "$repoRaw")" \
+            --arg fullpath "$repoRaw" \
+            '{
+				"title": $repo,
+				"subtitle": $fullpath,
+				"arg": $fullpath,
+				"icon": {"path": $fullpath, "type": "fileicon"},
+				"autocomplete": $repo,
+				"type": "file:skipcheck",
+				"text": $fullpath,
+				"quicklookurl": $fullpath
+			}'
+    )
+    json="$json,$newItem"
+done < <(echo "$allRepos")
+
+# Remove leading comma
+json="${json:1}"
+
+# Frame final JSON
+final="{\"items\": [$json]}"
+printf "%s" "$final"
+
+# Exit
+exit 0
