7366dd688e13d1f9b3d9977e5e928d82cfc57ad8
Jon Dayley <jonathandayley@gmail.com>
2025-09-27 06:46:40 -0600

    Continue

--- workflows/user.workflow.A8C3DF4B-9281-46E4-A60B-D548C8AA5CF8/options.py
+++ workflows/user.workflow.A8C3DF4B-9281-46E4-A60B-D548C8AA5CF8/options.py
@@ -2,12 +2,12 @@
 
 """Provides options to Alfred."""
 
-import os
 import re
 import sys
 from pathlib import Path
 from typing import NoReturn, cast, get_args
 
+from common.alfred_script_filter import ScriptFilterJson, send
 from common.write import err
 from universal import EditorId
 
@@ -22,28 +22,24 @@ def stop() -> NoReturn:
 
 
 # Validate argument count
-n_args = 2
+n_args = 3
 if len(sys.argv) != n_args:
     stop()
 
-# Assign editor ID argument
-id_editor_raw: str = sys.argv[1]
-if id_editor_raw not in get_args(EditorId):
-    stop()
-id_editor: EditorId = cast("EditorId", id_editor_raw)
-
-# Get repos directory
-dir_repos = Path(os.environ["REPOS_DIRECTORY"])
-
-# Make sure repos directory exists
+# Assign ad validate directory argument
+dir_repos = Path(sys.argv[1])
 if not dir_repos.exists():
     err(f"NOT FOUND: {dir_repos}")
     sys.exit(1)
-
-# Make sure repos directory is a directory
 if not dir_repos.is_dir():
     err(f"NOT A DIRECTORY: {dir_repos}")
     sys.exit(1)
+
+# Assign editor ID argument
+id_editor_raw: str = sys.argv[2]
+if id_editor_raw not in get_args(EditorId):
+    stop()
+id_editor: EditorId = cast("EditorId", id_editor_raw)
 
 # Define closure to decide which repos get shown
 match id_editor:
@@ -81,6 +77,28 @@ match id_editor:
             )
 
 
-# List repos to Alfred
+# Prepare Alfred output
 repos = [repo for repo in [Path(p) for p in Path.iterdir(dir_repos)] if repo.is_dir()]
-output = [repo for repo in repos if filter_repo(repo)]  # TODO: Continue
+output: ScriptFilterJson = {
+    "variables": {
+        "id_editor": id_editor,
+    },
+    "items": [
+        {
+            "title": repo.name,
+            "subtitle": str(repo),
+            "arg": [
+                str(repo),
+                "is_alfred"
+                if repo.name == "Alfred.alfredpreferences"
+                else "not_alfred",
+            ],
+            "icon": {"path": str(repo), "type": "fileicon"},
+        }
+        for repo in repos
+        if filter_repo(repo)
+    ],
+}
+
+# Send it
+send(output)
