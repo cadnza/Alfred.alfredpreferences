607e71342fd92ed1e916405492daece4457bc7f6
Jon Dayley <jonathandayley@gmail.com>
2025-10-05 06:34:07 -0600

    Add check in path script

---
+++ workflows/common/check_in_path.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Check whether the given binary is in the path, writing a simple banner to stdout and exiting non-0 if not
+command -v "$1" || {
+    /usr/bin/python3 -m alfred_script_filter.simple_banner \
+        "Missing '$1'" \
+        "Cannot find '$1' in \$PATH" \
+        '/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns' \
+        ''
+    exit 1
+}
