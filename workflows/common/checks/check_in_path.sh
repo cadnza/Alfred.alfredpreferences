e5b146ebc045b420e462e8a2bfce947dd006b155
Jon Dayley <jonathandayley@gmail.com>
2025-10-05 06:57:51 -0600

    Organize

--- workflows/common/checks/check_in_path.sh
+++ workflows/common/checks/check_in_path.sh
@@ -2,7 +2,7 @@
 
 # Check whether the given binary is in the path, writing a simple banner to stdout and exiting non-0 if not
 command -v "$1" >/dev/null || {
-    PYTHONPATH="$(dirname "$0")" /usr/bin/python3 -m alfred_script_filter.simple_banner \
+    PYTHONPATH="$(dirname "$0")/.." /usr/bin/python3 -m alfred_script_filter.simple_banner \
         "Missing '$1'" \
         "Cannot find '$1' in \$PATH" \
         '/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns' \
--- workflows/common/checks/check_installed.sh
+++ workflows/common/checks/check_installed.sh
@@ -2,7 +2,7 @@
 
 # Check whether the given app is installed, writing a simple banner to stdout and exiting non-0 if not
 [ -d "$2" ] || {
-    PYTHONPATH="$(dirname "$0")" /usr/bin/python3 -m alfred_script_filter.simple_banner \
+    PYTHONPATH="$(dirname "$0")/.." /usr/bin/python3 -m alfred_script_filter.simple_banner \
         "$1 not installed" \
         "Please install $1" \
         '/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns' \
