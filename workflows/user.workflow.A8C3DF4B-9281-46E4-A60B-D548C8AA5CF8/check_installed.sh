7bc1a17827ca95569455274de30981154732fb00
Jon Dayley <jonathandayley@gmail.com>
2025-10-05 06:52:50 -0600

    Add check installed

---
+++ workflows/user.workflow.A8C3DF4B-9281-46E4-A60B-D548C8AA5CF8/check_installed.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Check whether the given app is installed, writing a simple banner to stdout and exiting non-0 if not
+[ -d "$2" ] || {
+    PYTHONPATH="$(dirname "$0")/../common" /usr/bin/python3 -m alfred_script_filter.simple_banner \
+        "$1 not installed" \
+        "Please install $1" \
+        '/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns' \
+        ''
+    exit 1
+}
