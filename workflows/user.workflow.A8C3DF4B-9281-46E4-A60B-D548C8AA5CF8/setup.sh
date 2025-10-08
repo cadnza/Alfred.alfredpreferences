04f154304e68b5b5f6b4ad9d22fb8e59b994f9c2
Jon Dayley <jonathandayley@gmail.com>
2025-10-07 21:48:18 -0600

    Remove exits

--- workflows/user.workflow.A8C3DF4B-9281-46E4-A60B-D548C8AA5CF8/setup.sh
+++ workflows/user.workflow.A8C3DF4B-9281-46E4-A60B-D548C8AA5CF8/setup.sh
@@ -12,10 +12,10 @@ alfred_object_id="$4"
 [ -d common ] || ln -s "$(realpath ../common)" .
 
 # Ensure python3.11 is in path
-./common/checks/check_in_path.sh python3.11 || exit 0
+./common/checks/check_in_path.sh python3.11
 
 # Ensure app is installed
-./common/checks/check_installed.sh "$app_name" "$app_bundle_path" || exit 0
+./common/checks/check_installed.sh "$app_name" "$app_bundle_path"
 
 # Link icon
 ./common/link.sh "$alfred_object_id.png" "$app_icns_path"
