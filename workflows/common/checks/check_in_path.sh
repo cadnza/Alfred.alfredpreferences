ee0bc90de0194b40ed76d118133fdc40b9221340
Jon Dayley <jonathandayley@gmail.com>
2025-10-10 10:45:05 -0600

    Add log message for path

--- workflows/common/checks/check_in_path.sh
+++ workflows/common/checks/check_in_path.sh
@@ -2,6 +2,7 @@
 
 # Check whether the given binary is in the path, writing a simple banner to stdout and exiting non-0 if not
 command -v "$1" >/dev/null || {
+    echo "PATH: $PATH" >&2
     PYTHONPATH="$(dirname "$0")/.." "$(dirname "$0")/../alfred_script_filter/simple_banner.py" \
         "Missing $1" \
         "Cannot find \`$1\` in \$PATH" \
