60ae01e363b7f96c63a16e39d7809d72ee319601
Jon Dayley <jonathandayley@gmail.com>
2025-10-10 10:28:29 -0600

    Update banner

--- workflows/common/checks/check_in_path.sh
+++ workflows/common/checks/check_in_path.sh
@@ -3,8 +3,8 @@
 # Check whether the given binary is in the path, writing a simple banner to stdout and exiting non-0 if not
 command -v "$1" >/dev/null || {
     PYTHONPATH="$(dirname "$0")/.." "$(dirname "$0")/../alfred_script_filter/simple_banner.py" \
-        "Missing '$1'" \
-        "Cannot find '$1' in \$PATH" \
+        "Missing $1" \
+        "Cannot find \`$1\` in \$PATH" \
         '/System/Applications/Utilities/Terminal.app/Contents/Resources/Terminal.icns' \
         ''
     exit 8
